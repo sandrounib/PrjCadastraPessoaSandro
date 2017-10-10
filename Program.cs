2e0ada5b52d5016c9e79d9f531b34f45a1314c11
unknown <sandrounib@gmail.com>
2017-10-09 21:41:17 -0300

    User Registration

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrjCadastraPessoaSandro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Data = new string[10];
+            string [] Variable = new string[] {"name","adress","city","birthPlace","city","age","genre","civelState","rg","cpf"};
+            for (int i = 0; i < Data.Length; i++)
+            {
+                Console.WriteLine("Digete o seu/sua " + Variable[i]);
+                Data[i] = Console.ReadLine();
+            }
+            Console.Clear();
+            Console.WriteLine("Relatório");
+            for (int i = 0; i < Data.Length; i++)
+            {
+                Console.WriteLine(Variable[i] + ":" + Data[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
